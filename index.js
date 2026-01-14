fcce3afc73e884ab5e4c6875a1405ea1b0c08c4b
Ubuntu <ubuntu@ip-172-31-3-24.ap-south-1.compute.internal>
2026-01-14 11:24:57 +0000

    final cicd test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-  res.send("GitHub Actions CI/CD Working 🚀");
+  res.send("GitHub Actions CI/CD Successfully Working 🚀");
 });
 
 app.listen(3000, ()=> console.log("Server started on 3000"));
